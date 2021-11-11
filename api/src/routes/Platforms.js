2685c67ddde574b28678d7980bda09cb71b91ed1
rossymarin1 <rosymarin16@outlook.com>
2021-11-10 18:42:00 -0600

    Limpiando codigo

--- api/src/routes/Platforms.js
+++ api/src/routes/Platforms.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express();
 const controllerPlatforms = require('../controllers/Platforms');
-
 router.get('/',controllerPlatforms.getPlatforms);
-
 module.exports = router;
--- api/src/routes/Videogames.js
+++ api/src/routes/Videogames.js
@@ -1,10 +1,9 @@
 const {Router} = require('express');
 const router = Router();
 const videogames = require('../controllers/Videogames.js')
-const {Key} = process.env;
-router.get('/',videogames.getVideogames);//funciona
-router.get('/:id',videogames.getVideogame);//funciona
-router.post('/',videogames.insertVideogame);//funciona
+router.get('/',videogames.getVideogames);
+router.get('/:id',videogames.getVideogame);
+router.post('/',videogames.insertVideogame);
 module.exports = router;
 
 
